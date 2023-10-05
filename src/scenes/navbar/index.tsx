a69be305d3cddcebe2e4028761591a3060b167d1
iamlydial <lydia.lavecchia@gmail.com>
2023-10-05 14:37:18 +0200

    added mobile menu shadown when page scroll is more than 0

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -10,21 +10,22 @@ import ActionButton from "@/shared/ActionButton";
 
 
 type Props = {
+    isTopOfPage: boolean;
     selectedPage: SelectedPage;
     setSelectedPage: (value: string) => void;
 }
 
-const Navbar = ({selectedPage, setSelectedPage}: Props) => {
+const Navbar = ({isTopOfPage, selectedPage, setSelectedPage}: Props) => {
     const flexBetween= "flex items-center justify-between";
     const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false)
-
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
+    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow"
 
   return (
     <nav>
-        <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
-<div className={`${flexBetween} mx-auto w-5/6`}>
-<div className={`${flexBetween} w-full gap-16`}>
+        <div className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
+            <div className={`${flexBetween} mx-auto w-5/6`}>
+            <div className={`${flexBetween} w-full gap-16`}>
     {/* left side */}
     <img alt="logo" src={Logo}/>
     {/* right side */}
@@ -58,6 +59,13 @@ const Navbar = ({selectedPage, setSelectedPage}: Props) => {
                         <button onClick={()=> setIsMenuToggled(!isMenuToggled)}>
                             <XMarkIcon className="h-6 w-6 text-gray-400"/>
                         </button>
+                    </div>
+                    {/* MENU ITEMS */}
+                    <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+                        <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                        <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                        <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+                        <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
                     </div>
                 </div>
             )}
