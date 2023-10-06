07a6bf4e8b63c80f2db1ab3a6d36218977d18874
iamlydial <lydia.lavecchia@gmail.com>
2023-10-06 11:05:29 +0200

    added benefit animation

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -25,6 +25,13 @@ const benefits: Array<BenefitType> = [
     },
 ]
 
+const container = {
+    hidden:{},
+    visible: {
+        transition: { staggerChildren: 0.2}
+    }
+}
+
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
 }
@@ -45,9 +52,14 @@ const Benefits = ({setSelectedPage}: Props) => {
                </p>
             </div>
             {/* BENEFITS */}
-            <div className="md:flex items-center justify-between gap-8 mt-5">
+            <motion.div 
+            className="md:flex items-center justify-between gap-8 mt-5"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once:true, amount: 0.5}}
+            variants={container}>
                 {benefits.map((benefit: BenefitType)=> (<Benefit key={benefit.title} icon={benefit.icon} title={benefit.title} description={benefit.description} setSelectedPage={setSelectedPage}/>))}
-            </div>
+            </motion.div>
         </motion.div>
     </section>
   )
